3082ce386d675049935da2639ddd93596d91fd89
Aric Crosson Bouwers <aricbouwers@outlook.com>
2023-06-07 15:45:30 -0600

    removing frontend to reinit without tailwindcss

---
+++ frontend/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/acbouwers/_group-projects/unstuck/frontend/app/layout.tsx": [
+      "/Users/acbouwers/_group-projects/unstuck/frontend/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/acbouwers/_group-projects/unstuck/frontend/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/acbouwers/_group-projects/unstuck/frontend/app/page": [
+      "/Users/acbouwers/_group-projects/unstuck/frontend/app/globals.css",
+      "/Users/acbouwers/_group-projects/unstuck/frontend/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
