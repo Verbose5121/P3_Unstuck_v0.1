38c5e37d0115423079fd6d792faa762fc028ecff
MK <thisisformed@pm.me>
2023-06-18 20:39:05 -0600

    UN-7 #comment connect signup to SB

--- frontend/src/app/(signup)/signup/page.tsx
+++ frontend/src/app/(signup)/signup/page.tsx
@@ -10,8 +10,28 @@ import {
 } from "@chakra-ui/react";
 import { Stack, HStack, VStack } from "@chakra-ui/react";
 import { Box, Center, Link, Button, Card, Input } from "@chakra-ui/react";
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+import type { Database } from "../../../lib/database.types";
 
 export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const router = useRouter()
+  const supabase = createClientComponentClient<Database>()
+
+  const handleSignUp = async () => {
+    await supabase.auth.signUp({
+      email, 
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/auth/callback`,
+      },
+    })
+    router.refresh()
+  }
   return (
     <div style={{ margin: "5rem" }}>
       <Card>
@@ -26,6 +46,7 @@ export default function Signup() {
                 style={{ textAlign: "center" }}
                 placeholder="Email"
                 size="lg"
+                onChange={(e) => setEmail(e.target.value)} value={email}
               />
               <FormLabel>Password</FormLabel>
               <Input
@@ -33,12 +54,16 @@ export default function Signup() {
                 placeholder="Password"
                 size="lg"
                 type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
               />
               <Link href="/onboarding">
                 <Button
                   colorScheme="teal"
                   variant="outline"
-                  size="lg">
+                  size="lg"
+                  onClick={handleSignUp}
+                  >
                   {" "}
                   Submit
                 </Button>
@@ -56,7 +81,7 @@ export default function Signup() {
                 </p>
               </CardFooter>
             </VStack>
-          </FormControl>
+          </FormControl>      
         </CardBody>
       </Card>
     </div>
