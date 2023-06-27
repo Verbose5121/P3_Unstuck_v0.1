0ce851bc334aa1ef1c876f7b2a8a1180f59c2bb5
Aric Crosson Bouwers <aricbouwers@outlook.com>
2023-06-27 01:42:11 -0600

    moved middleware to the proper folder to actually work

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -10,20 +10,30 @@ export async function middleware(req: NextRequest) {
   const {
     data: { user },
   } = await supabase.auth.getUser();
+  console.log("middleware is running");
+  console.log("user from middleware", user);
 
   // if user is signed in and the current path is / redirect the user to /account
-  if (user && req.nextUrl.pathname === "/") {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
+  if (
+    (user && req.nextUrl.pathname === "/") ||
+    (user && req.nextUrl.pathname === "/login") ||
+    (user && req.nextUrl.pathname === "/signup")
+  ) {
+    return NextResponse.redirect(new URL("/student-dashboard", req.url));
   }
 
   // if user is not signed in and the current path is not / redirect the user to /
-  if (!user && req.nextUrl.pathname !== "/") {
-    return NextResponse.redirect(new URL("/", req.url));
+  if (
+    (!user && req.nextUrl.pathname !== "/") ||
+    (!user && req.nextUrl.pathname === "/login") ||
+    (!user && req.nextUrl.pathname === "/signup")
+  ) {
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 
   return res;
 }
 
 export const config = {
-  matcher: ["/", "/dashboard"],
+  matcher: ["/", "/student-dashboard"],
 };
