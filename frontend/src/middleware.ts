631f55e71a290e6be1f347a877cec228a60fa696
Aric Crosson Bouwers <aricbouwers@outlook.com>
2023-06-28 09:51:41 -0600

    fixed the race condition i think - it's the middleware checking if user is NOT logged in that's causing issues

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -13,19 +13,19 @@ export async function middleware(req: NextRequest) {
   console.log("middleware is running");
   console.log("user from middleware", user);
 
-  // if user is signed in and the current path is / redirect the user to /account
+  // // if user is signed in and the current path is / redirect the user to /account
   if (user && req.nextUrl.pathname === "/login") {
     return NextResponse.redirect(new URL("/student-dashboard", req.url));
   }
   if (user && req.nextUrl.pathname === "/signup") {
     return NextResponse.redirect(new URL("/student-dashboard", req.url));
   }
-  if (!user && req.nextUrl.pathname === "/student-dashboard") {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-  if (!user && req.nextUrl.pathname === "/profile") {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+  // if (!user && req.nextUrl.pathname === "/student-dashboard") {
+  //   return NextResponse.redirect(new URL("/login", req.url));
+  // }
+  // if (!user && req.nextUrl.pathname === "/profile") {
+  //   return NextResponse.redirect(new URL("/login", req.url));
+  // }
 
   return res;
 }
